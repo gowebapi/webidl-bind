ca90f77bb0ab9ca52b5c6f3af951e7c80a2b00ad
Martin Juhlin <martin.juhlin@gmail.com>
2019-02-08 10:39:18 +0100

    changing path in run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-go run main.go -single-package all -log-warning=false -output tmp `find ../idl/webapi -type f` && (cd tmp/all && GOOS=js GOARCH=wasm go build )
+go run main.go -single-package webapiall -log-warning=false -output ../webapi `find ../idl/webapi -type f` && (cd tmp/all && GOOS=js GOARCH=wasm go build )
